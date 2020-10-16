449ea2de0819fe714fcb3b86474b436055f23993
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-16 17:39:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,18 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
-  end 
+def roll_call_dwarves(d)
+  d.each_with_index {|d, i|puts "#{i+1}. #{d}"}
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |planeteer_call|
-    "#{planeteer_call.capitalize}!"
-  end
+def summon_captain_planet(calls)
+  calls.map {|c| "#{c.capitalize}!"}
 end
 
 def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4
-  end
+  calls.any? {|c| c.length > 4}
 end
 
 def find_the_cheese(snacks)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses = ["cheddar", "gouda", "camembert"]
   snacks.find do |snack|
-    cheese_types.include?(snack)
+    cheeses.include?(snack)
   end
 end
