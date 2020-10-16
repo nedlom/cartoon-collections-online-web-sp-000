a6ce79ee3a1cadd6f25693075bffb6aef2f50867
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-16 17:34:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4
-  end
+  calls.any? {|c| c.length > 4}
 end
 
 def find_the_cheese(snacks)
